23cda5be2269f80074a80179f72efba989f88f38
edean78 <edean78@gmail.com>
2019-10-28 21:49:34 -0400

    Added code to call and retrieve movie information using omdb api

--- liri.js
+++ liri.js
@@ -7,7 +7,7 @@ var moment = require('moment');
 var Spotify = require('node-spotify-api');
 
 // Code required to import the keys.js file and store it in a variable
-var keys = require("./keys.js/index.js");
+var keys = require("./keys.js");
 
 // Able to access keys for spotify
 var spotify = new Spotify(keys.spotify);
@@ -16,12 +16,12 @@ var spotify = new Spotify(keys.spotify);
 var command = process.argv[2];
 
 // Search variable to hold the concert, movie, song entered by the user
-var entSearch = process.argv.slice(3).join(" ");
+var entSearch = process.argv.slice(3).join(" ").trim();
 
 // Create switch statment so when command is used the corresponding function is run
 switch (command) {
     case "concert-this":
-        concertThis();
+        concertThis(entSearch);
         break;
     case "spotify-this-song":
         spotifyThis();
@@ -34,28 +34,79 @@ switch (command) {
         break;
 }
 
-// function concertThis(){
+function concertThis() {
 
-// }
+}
 
-function spotifyThis(){
-    if (!entSearch){
+function spotifyThis() {
+    if (!entSearch) {
         entSearch = "All the Small Things"
     }
-    spotify.search({ type: 'track', query: entSearch }, function(err, data) {
+    spotify.search({ type: 'track', query: entSearch, limit: 5 }, function (err, data) {
         if (err) {
-          return console.log('Error occurred: ' + err);
+            return console.log('Error occurred: ' + err);
         }
-       
-    //   var response = JSON.stringify(data, null, 2); 
-      console.log(JSON.stringify(data, null, 2)); 
-      });
+
+        var info = data.tracks.items[0];
+
+        console.log("Artist: " + info.artists.name);
+        console.log("Song Name: " + info.name);
+        console.log("Preview: " + info.preview_url);
+        console.log("Album: " + info.album.name);
+        console.log("------------------------------");
+    });
 };
 
-// function movieThis(){
+function movieThis() {
+    if (!entSearch) {
+        entSearch = "Mr Nobody"
+    }
 
-// }
+    var queryUrl = "http://www.omdbapi.com/?t=" + entSearch + "&y=&plot=short&apikey=trilogy";
 
-// function doWhat(){
+    axios
+        .get(queryUrl)
+        .then(function (response) {
 
-// }
+            var movieInfo = response.data;
+            var movieTitle = movieInfo.Title;
+            var movieYear = movieInfo.Year;
+            var movieIMDB = movieInfo.Ratings[0].Value;
+            var movieRT = movieInfo.Ratings[1].Value;
+            var movieCountry = movieInfo.Country;
+            var movieLanguage = movieInfo.Language;
+            var moviePlot = movieInfo.Plot;
+            var movieActors = movieInfo.Actors;
+
+            // If the axios was successful...
+            // Then log the body from the site!
+            // console.log(response.data);
+            console.log("--------------------------------------\n");
+            console.log(`Title: ${movieTitle}\nYear movie was released: ${movieYear}\nIMDB Rating: ${movieIMDB}\nRotten Tomatoes Rating: ${movieRT}\nCountry: ${movieCountry}\nLanguage: ${movieLanguage}\nPlot: ${moviePlot}\nActors: ${movieActors}`);
+            console.log("--------------------------------------");
+        })
+        .catch(function (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log("---------------Data---------------");
+                console.log(error.response.data);
+                console.log("---------------Status---------------");
+                console.log(error.response.status);
+                console.log("---------------Status---------------");
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
+}
+
+function doWhat() {
+
+}
